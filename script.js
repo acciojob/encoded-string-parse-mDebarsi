e15d15451a7a8fbc1abbc256f9364b086a633350
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-12-16 21:20:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const parseCode = (str) => {
   // your code here
+	 // Split the encoded string by zeros
+  const parts = Str.split("0");
+
+  // Extract firstName, lastName, and id from the parts array
+  const firstName = parts[0];
+  const lastName = parts[1];
+  const id = parts[2];
+
+  // Create and return the result object
+  const result = {
+    firstName: firstName,
+    lastName: lastName,
+    id: id
+  };
+
+  return result;
 };
 
 // Do not change the code below
